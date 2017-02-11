cba67a3d541f911991ed96b42da6e7155f409344
Ranmocy Sheng <ranmocy@gmail.com>
2017-02-11 09:00:11 -0800

    add Fetch webidl

---
+++ js/Fetch.js
@@ -0,0 +1,8 @@
+/**
+ * typedef (sequence<sequence<ByteString>> or record<ByteString, ByteString>) HeadersInit;
+ */
+/**
+ * @typedef {(ByteString[][]|Object.<ByteString, ByteString>)} HeadersInit
+ */
+
+
